e1f7c80a548cf456f78eee4955776829927de9e1
bq-devendra-30 <devendra.patidar@betaqu.com>
2024-01-04 18:42:52 +0530

    datatype

---
+++ Basics/datatype.js
@@ -0,0 +1,11 @@
+// boolean => true/false
+//number 
+// string => ""
+// bigint
+// null => standalone value
+//undefined 
+//symbol => unique
+//object =>
+
+console.log(typeof null); //object
+console.log(undefined); //undefined
---
+++ Basics/test1.js
@@ -0,0 +1,8 @@
+const accountId = 1234 
+let accountEmail = "devpatel4627@gmail.com"
+
+/*
+prefferd not to use "ver"
+*/
+
+console.log([accountId, accountEmail]);
